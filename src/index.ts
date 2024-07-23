929678adb53e69639b1a6dbdb75e81d6abedadb0
itzdrli <liletian1231@outlook.com>
2024-07-23 14:37:04 +0000

    Hi

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,24 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command("surl <url>", "生成短链接", { authority: 0 })
+    .action(async ({ session }, url) => {
+      if (!url) return "请输入链接"
+      if (!url.startsWith("http")) {
+        return "请提供正确的链接 (http:// 或 https:// 开头)"
+      }
+
+      const response = await fetch('https://l-i.biz/shorten', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: url })
+      });
+      if (!response) {
+        return "请求失败"
+      }
+      const data = await response.json()
+      return data.url
+    })
 }
